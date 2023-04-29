c6366bf23e41b04a876e80a75aa77b2294527543
nikopermi <nikopermilovsky@gmail.com>
2023-04-29 11:40:06 -0700

    changed links

--- site/src/components/Carousel.js
+++ site/src/components/Carousel.js
@@ -3,40 +3,42 @@ import Image from 'next/image'
 import { Carousel } from "react-responsive-carousel";
 import styles from "@/styles/Carousel.module.css"
 
-
 const CustomCarousel = (props) => (
   <div className={styles.wrapper}>
     <Carousel autoPlay>
       <div>
         <img
           alt=""
-          src={`http://localhost:3000/maple-hall/${props.dorm}1.jpg`}
+          src={`https://raw.githubusercontent.com/DormWiki/dorm-wiki/main/site/public/${props.dorm}/${props.dorm}1.jpg`}
         />
         <p className="legend">Legend 1</p>
       </div>
       <div>
         <img
           alt=""
-          src={`http://localhost:3000/maple-hall/${props.dorm}2.jpg`}
+          src={`https://raw.githubusercontent.com/DormWiki/dorm-wiki/main/site/public/${props.dorm}/${props.dorm}2.jpg`}
         />
         <p className="legend">Legend 2</p>
       </div>
       <div>
         <img
           alt=""
-          src={`http://localhost:3000/maple-hall/${props.dorm}3.jpg`}
+          src={`https://raw.githubusercontent.com/DormWiki/dorm-wiki/main/site/public/${props.dorm}/${props.dorm}3.jpg`}
         />
         <p className="legend">Legend 3</p>
       </div>
       <div>
         <img
           alt=""
-          src={`http://localhost:3000/maple-hall/${props.dorm}4.jpg`}
+          src={`https://raw.githubusercontent.com/DormWiki/dorm-wiki/main/site/public/${props.dorm}/${props.dorm}4.jpg`}
         />
         <p className="legend">Legend 4</p>
       </div>
       <div>
-        <img alt="" src={`http://localhost:3000/maple-hall/${props.dorm}5.jpg`}/>
+        <img
+          alt=""
+          src={`https://raw.githubusercontent.com/DormWiki/dorm-wiki/main/site/public/${props.dorm}/${props.dorm}5.jpg`}
+        />
         <p className="legend">Legend 5</p>
       </div>
     </Carousel>
