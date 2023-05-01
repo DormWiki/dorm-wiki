81b941aeddd91ee60fff534261f88c931a1bbbc9
nikopermi <nikopermilovsky@gmail.com>
2023-04-30 23:23:18 -0700

    cleaned sidebar, added cool transition. cropped logo

--- site/src/pages/wiki.js
+++ site/src/pages/wiki.js
@@ -8,39 +8,116 @@ import React, { useState } from "react";
 export default function Wiki() {
   return (
     <>
-      <div className={styles.logo}>
-        <div className={styles.navbar_logo_wrapper}>
-            <img src="/dw-logo-navbar.png"></img>
-        </div>
-        <div className={styles.navbar}>
-            <ul>
-                <li>
-                    <Link a href="/">Home</Link>
-                </li>
-                <li>
-                    <Link href="/events">Events</Link>
-                </li>
-                <li>
-                    <Link href="/wiki">Wiki</Link>
-                </li>
-                <li>
-                    <Link href="/about">About</Link>
-                </li>
-                <li>
-                    <Link href="/search">Search</Link>
-                </li>
-                <button type="button" onClick={() => router.push('/login')}>Login</button>
-            </ul>
-        </div>
+      <div className={styles.navbar_logo_wrapper}>
+        <img src="/dw-logo-navbar.png"></img>
+      </div>
+      <div className={styles.navbar}>
+        <ul>
+          <li>
+            <Link a href="/">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link href="/events">Events</Link>
+          </li>
+          <li>
+            <div className={styles.dropdown}>
+              <Link href="/wiki">Wiki</Link>
+              <div className={styles.dropdown_text}>
+                <Link href="/wiki/residence-halls">Residence halls</Link>
+                <Link href="/wiki/academic-apts">Academic-year apartments</Link>
+                <Link href="/wiki/year-apts">Full-year apartments</Link>
+                <Link href="/wiki/family-apts">Family apartments</Link>
+              </div>
+            </div>
+          </li>
+          <li>
+            <Link href="/about">About</Link>
+          </li>
+          <li>
+            <Link href="/search">Search</Link>
+          </li>
+          <button type="button" onClick={() => router.push("/login")}>
+            Login
+          </button>
+        </ul>
       </div>
       <div className={styles.sidebar}>
         <ul>
-          <li>
-            <Link href="#">Link</Link>
-          </li>
-          <li>
-            <Link href="#">Link</Link>
-          </li>
+          <h2>North Campus</h2>
+          <div className={styles.sidebar_sub}>
+            <li>
+              <Link href="/wiki/dorms/hansee-hall">Hansee Hall</Link>
+            </li>
+            <li>
+              <Link href="/wiki/dorms/madrona-hall">Madrona Hall</Link>
+            </li>
+            <li>
+              <Link href="/wiki/dorms/mccarty-hall">McCarty Hall</Link>
+            </li>
+            <li>
+              <Link href="/wiki/dorms/mccahon-hall">McCahon Hall</Link>
+            </li>
+            <li>
+              <Link href="/wiki/dorms/oak-hall">Oak Hall</Link>
+            </li>
+            <li>
+              <Link href="/wiki/dorms/willow-hall">Willow Hall</Link>
+            </li>
+          </div>
+          <h2>West Campus</h2>
+          <div className={styles.sidebar_sub}>
+            <li>
+              <Link href="/wiki/dorms/alder-hall">Alder Hall</Link>
+            </li>
+            <li>
+              <Link href="/wiki/dorms/elm-hall">Elm Hall</Link>
+            </li>
+            <li>
+              <Link href="/wiki/dorms/lander-hall">Lander Hall</Link>
+            </li>
+            <li>
+              <Link href="/wiki/dorms/maple-hall">
+               Maple Hall
+              </Link>
+            </li>
+            <li>
+              <Link href="/wiki/dorms/poplar-hall">Poplar Hall</Link>
+            </li>
+            <li>
+              <Link href="/wiki/dorms/terry-hall">Terry Hall</Link>
+            </li>
+            <li>
+              <Link href="/wiki/dorms/mercer-court">Mercer Court</Link>
+            </li>
+            <li>
+              <Link href="/wiki/dorms/stevens-court">Stevens Court</Link>
+            </li>
+            <li>
+              <Link href="/wiki/dorms/cedar-apartments">Cedar Apartments</Link>
+            </li>
+          </div>
+          <h2>Off Campus</h2>
+          <div className={styles.sidebar_sub}>
+            <li>
+              <Link href="/wiki/dorms/commodore-duchess">
+                Commodore Duchess
+              </Link>
+            </li>
+            <li>
+              <Link href="/wiki/dorms/nordheim-court">Nordheim Court</Link>
+            </li>
+            <li>
+              <Link href="/wiki/dorms/radford-court">Radford Court</Link>
+            </li>
+            <li>
+              <Link href="/wiki/dorms/blakeley-village">Blakeley Village</Link>
+            </li>
+            <li>
+              <Link href="/wiki/dorms/laurel-village">Laural Village</Link>
+            </li>
+          </div>
         </ul>
       </div>
       <section className={styles.content}>
@@ -65,7 +142,7 @@ export default function Wiki() {
           </li>
           <li>
             <Link href="/wiki/dorms/maple-hall">
-              &#8594;Maple Hall &#8592; ONLY ONE THAT WORKS
+             Maple Hall
             </Link>
           </li>
           <li>
--- site/src/pages/wiki/dorms/[dorm].js
+++ site/src/pages/wiki/dorms/[dorm].js
@@ -25,6 +25,9 @@ export default function Wiki( {info} ) {
         <link rel="icon" href="/dw-logo-icon.png" />
       </Head>
       <main className={styles.main}>
+        <div className={styles.navbar_logo_wrapper}>
+          <img src="/dw-logo-navbar.png"></img>
+        </div>
         <div className={styles.navbar}>
           <ul>
             <li>
@@ -36,7 +39,17 @@ export default function Wiki( {info} ) {
               <Link href="/events">Events</Link>
             </li>
             <li>
-              <Link href="/wiki">Wiki</Link>
+              <div className={styles.dropdown}>
+                <Link href="/wiki">Wiki</Link>
+                <div className={styles.dropdown_text}>
+                  <Link href="/wiki/residence-halls">Residence halls</Link>
+                  <Link href="/wiki/academic-apts">
+                    Academic-year apartments
+                  </Link>
+                  <Link href="/wiki/year-apts">Full-year apartments</Link>
+                  <Link href="/wiki/family-apts">Family apartments</Link>
+                </div>
+              </div>
             </li>
             <li>
               <Link href="/about">About</Link>
@@ -44,20 +57,90 @@ export default function Wiki( {info} ) {
             <li>
               <Link href="/search">Search</Link>
             </li>
+            <button type="button" onClick={() => router.push("/login")}>
+              Login
+            </button>
           </ul>
-          <button type="button">Login</button>
         </div>
         <div className={styles.sidebar}>
-          <div className={styles.logoside}>
-            <img src="/dw-logo-side.png" style={{width:125}}></img>
-          </div>
           <ul>
-            <li>
-              <Link href="#">Link</Link>
-            </li>
-            <li>
-              <Link href="#">Link</Link>
-            </li>
+            <h2>North Campus</h2>
+            <div className={styles.sidebar_sub}>
+              <li>
+                <Link href="/wiki/dorms/hansee-hall">Hansee Hall</Link>
+              </li>
+              <li>
+                <Link href="/wiki/dorms/madrona-hall">Madrona Hall</Link>
+              </li>
+              <li>
+                <Link href="/wiki/dorms/mccarty-hall">McCarty Hall</Link>
+              </li>
+              <li>
+                <Link href="/wiki/dorms/mccahon-hall">McCahon Hall</Link>
+              </li>
+              <li>
+                <Link href="/wiki/dorms/oak-hall">Oak Hall</Link>
+              </li>
+              <li>
+                <Link href="/wiki/dorms/willow-hall">Willow Hall</Link>
+              </li>
+            </div>
+            <h2>West Campus</h2>
+            <div className={styles.sidebar_sub}>
+              <li>
+                <Link href="/wiki/dorms/alder-hall">Alder Hall</Link>
+              </li>
+              <li>
+                <Link href="/wiki/dorms/elm-hall">Elm Hall</Link>
+              </li>
+              <li>
+                <Link href="/wiki/dorms/lander-hall">Lander Hall</Link>
+              </li>
+              <li>
+                <Link href="/wiki/dorms/maple-hall">
+                 Maple Hall
+                </Link>
+              </li>
+              <li>
+                <Link href="/wiki/dorms/poplar-hall">Poplar Hall</Link>
+              </li>
+              <li>
+                <Link href="/wiki/dorms/terry-hall">Terry Hall</Link>
+              </li>
+              <li>
+                <Link href="/wiki/dorms/mercer-court">Mercer Court</Link>
+              </li>
+              <li>
+                <Link href="/wiki/dorms/stevens-court">Stevens Court</Link>
+              </li>
+              <li>
+                <Link href="/wiki/dorms/cedar-apartments">
+                  Cedar Apartments
+                </Link>
+              </li>
+            </div>
+            <h2>Off Campus</h2>
+            <div className={styles.sidebar_sub}>
+              <li>
+                <Link href="/wiki/dorms/commodore-duchess">
+                  Commodore Duchess
+                </Link>
+              </li>
+              <li>
+                <Link href="/wiki/dorms/nordheim-court">Nordheim Court</Link>
+              </li>
+              <li>
+                <Link href="/wiki/dorms/radford-court">Radford Court</Link>
+              </li>
+              <li>
+                <Link href="/wiki/dorms/blakeley-village">
+                  Blakeley Village
+                </Link>
+              </li>
+              <li>
+                <Link href="/wiki/dorms/laurel-village">Laural Village</Link>
+              </li>
+            </div>
           </ul>
         </div>
         <section className={styles.content}>
