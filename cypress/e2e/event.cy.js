bbe809e23ac6e65a28c5a1e621e93e09c1789bff
jaylynnn <jzhang3107@gmail.com>
2023-05-31 00:38:36 -0700

    made some change to event testing

--- cypress/e2e/event.cy.js
+++ cypress/e2e/event.cy.js
@@ -1,10 +1,10 @@
-// Instruction:
-// Modify the event fields from line 14-19 to change to a new event
-// 200: new event is successfully posted and can be found on events 
-//		page and form page
-// 400: if any error occurs
-
-
+/*
+Post event: Manual Testing
+Modify the event fields from line 16-21 to change to a new event
+200: new event is successfully posted and can be found on events 
+		page and form page
+400: if any error occurs
+*/
 
 describe('Event Test', () => {
 	it('Test posting new event', () => {
@@ -34,12 +34,6 @@ describe('Event Test', () => {
 
 		// Expected: the new Event should show up:
 		// 1. in the event page
-		cy.contains(event.text);
-		cy.contains(event.organizer);
-		cy.contains(location);
-
-		//2. the correponding dorm page
-		cy.visit('http://localhost:3000/wiki/dorms/' + event.dorm.split(" ").join("-").toLowerCase())
 		cy.contains(event.text);
 		cy.contains(event.organizer);
 		cy.contains(location);
