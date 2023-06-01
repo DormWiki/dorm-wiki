da06a0f7573d794b127b349ff99e90844713653a
jaylynnn <jzhang3107@gmail.com>
2023-05-31 18:11:34 -0700

    removed colon from file name

--- cypress/e2e/loginTest.cy.js
+++ cypress/e2e/loginTest.cy.js
@@ -1,17 +1,18 @@
 describe('Google', function () {
-	beforeEach(function () {
-	//   cy.task('db:seed')
-	cy.visit('http://localhost:3000')
-	  cy.loginByGoogleApi()
-	})
+	// beforeEach(function () {
+	// //   cy.task('db:seed')
+	// cy.visit('http://localhost:3000')
+	//   cy.loginByGoogleApi()
+	// })
   
-	it('shows onboarding', function () {
-	  cy.contains('Welcome').should('be.visible')
-	})
-
 	// it('shows onboarding', function () {
-	// 	cy.visit('http://localhost:3000/api/auth/callback/google');
-	// 	cy.get('button').click()
+	//   cy.contains('Welcome').should('be.visible')
+	// })
 
-	//   })
+	it('shows onboarding', function () {
+		cy.loginByGoogleApi()
+		// cy.visit('http://localhost:3000/api/auth/callback/google');
+		// cy.get('button[className="google_button"]').click()
+
+	  })
   })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -2,22 +2,25 @@ Cypress.Commands.add('loginByGoogleApi', () => {
 	cy.log('Logging in to Google')
 	cy.request({
 	  method: 'POST',
-	  url: 'https://www.googleapis.com/oauth2/v4/token',
+	  url: 'http://localhost:3000/api/auth/callback/google',
 	  body: {
 		grant_type: 'refresh_token',
-		client_id: Cypress.env('googleClientId'),
-		client_secret: Cypress.env('googleClientSecret'),
-		refresh_token: Cypress.env('googleRefreshToken'),
+		// client_id: Cypress.env('googleClientId'),
+		// client_secret: Cypress.env('googleClientSecret'),
+		// refresh_token: Cypress.env('googleRefreshToken'),
+		client_id: '330949585590-2e6pm4judqk693avjvspavummmhjlfi4.apps.googleusercontent.com',
+		client_secret: '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',
+		refresh_token: '1//04DYbT0utZfzeCgYIARAAGAQSNgF-L9Irih_sYOVEKvJt5Z6_vUxE3fYrXO0GHDz86MhO8VdbrxfgTb8rTNilwx34Y9H89OiR9g',
 	  },
 	}).then(({ body }) => {
 	  const { access_token, id_token } = body
   
 	  cy.request({
 		method: 'GET',
-		url: 'https://www.googleapis.com/oauth2/v3/userinfo',
+		url: 'http://localhost:3000/api/auth/callback/google',
 		headers: { Authorization: `Bearer ${access_token}` },
 	  }).then(({ body }) => {
-		cy.log(body)
+		cy.log(body.email)
 		const userItem = {
 		  token: id_token,
 		  user: {
@@ -30,7 +33,9 @@ Cypress.Commands.add('loginByGoogleApi', () => {
 		}
   
 		window.localStorage.setItem('googleCypress', JSON.stringify(userItem))
-		cy.visit('/')
+		cy.visit('localhost:3000/user')
+		// cy.visit('http://localhost:3000/api/auth/callback/google')
+		// cy.visit('localhost:3000/user')
 	  })
 	})
   })
