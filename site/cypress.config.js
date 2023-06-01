2f5a8b01fc8955c2873d50995dd980141c07bb09
nikopermi <nikopermilovsky@gmail.com>
2023-05-31 22:52:53 -0700

    moved folders

--- site/cypress.config.js
+++ site/cypress.config.js
@@ -1,9 +1,23 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  component: {
+    devServer: {
+      framework: "react",
+      bundler: "webpack",
+    },
+  },
+
   e2e: {
+    baseUrl: 'http://localhost:3000',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+  },
+
+  env: {
+    googleRefreshToken: process.env.GOOGLE_REFRESH_TOKEN,
+    googleClientId: process.env.REACT_APP_GOOGLE_CLIENTID,
+    googleClientSecret: process.env.REACT_APP_GOOGLE_CLIENT_SECRET,
   },
 });
--- site/cypress/support/commands.js
+++ site/cypress/support/commands.js
@@ -1,25 +1,41 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add('loginByGoogleApi', () => {
+	cy.log('Logging in to Google')
+	cy.request({
+	  method: 'POST',
+	  url: 'http://localhost:3000/api/auth/callback/google',
+	  body: {
+		grant_type: 'refresh_token',
+		// client_id: Cypress.env('googleClientId'),
+		// client_secret: Cypress.env('googleClientSecret'),
+		// refresh_token: Cypress.env('googleRefreshToken'),
+		client_id: '330949585590-2e6pm4judqk693avjvspavummmhjlfi4.apps.googleusercontent.com',
+		client_secret: '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',
+		refresh_token: '1//04DYbT0utZfzeCgYIARAAGAQSNgF-L9Irih_sYOVEKvJt5Z6_vUxE3fYrXO0GHDz86MhO8VdbrxfgTb8rTNilwx34Y9H89OiR9g',
+	  },
+	}).then(({ body }) => {
+	  const { access_token, id_token } = body
+  
+	  cy.request({
+		method: 'GET',
+		url: 'http://localhost:3000/api/auth/callback/google',
+		headers: { Authorization: `Bearer ${access_token}` },
+	  }).then(({ body }) => {
+		cy.log(body.email)
+		const userItem = {
+		  token: id_token,
+		  user: {
+			googleId: body.sub,
+			email: body.email,
+			givenName: body.given_name,
+			familyName: body.family_name,
+			imageUrl: body.picture,
+		  },
+		}
+  
+		window.localStorage.setItem('googleCypress', JSON.stringify(userItem))
+		cy.visit('localhost:3000/user')
+		// cy.visit('http://localhost:3000/api/auth/callback/google')
+		// cy.visit('localhost:3000/user')
+	  })
+	})
+  })
